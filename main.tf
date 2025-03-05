73b2d2adedb8c3792c3be437321237b01723d60f
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-05 21:13:01 +0700

    destroyed all the resources

--- main.tf
+++ main.tf
@@ -6,21 +6,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_instance" "server" {
-  ami = "ami-0b03299ddb99998e9"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Main VPC"
-  }
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-southeast-1" 
-}
