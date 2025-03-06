779ac375ed37ca5e2148e5b5d712a35b1ec9eab9
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-06 15:50:39 +0700

    created terraform.tfvars file

--- variables.tf
+++ variables.tf
@@ -2,23 +2,19 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-southeast-1"
 }
 
 variable "az1" {
   description = "Availability zone 1"
   type        = string
-  default     = "ap-southeast-1a"
 }
 
 variable "vpc_cidr_block" {
   description = "VPC CIDR block"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
   description = "Subnet CIDR block"
   type        = string
-  default     = "10.0.10.0/24"
 }
