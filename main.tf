088fec84a491c7a897507212b41c8d54e3335f49
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-06 19:47:56 +0700

    added IGW and RT

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name = "Main VPC"
+    "Name" = "Main VPC"
   }
 }
 
@@ -24,5 +24,24 @@ resource "aws_subnet" "web" {
   availability_zone = var.az1
   tags = {
     "Name" = "Web Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_web_igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name" = "Main VPC IGW"
+  }
+}
+
+resource "aws_default_route_table" "main_vpc_default_rt" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_web_igw.id
+  }
+  tags = {
+    "Name" = "My Default Route Table"
   }
 }
