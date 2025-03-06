28438b26e58938542134a679f2836bfd728c32b5
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-06 15:12:23 +0700

    added subnet

--- main.tf
+++ main.tf
@@ -6,3 +6,29 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Main VPC"
+  }
+}
+
+resource "aws_subnet" "web" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    "Name" = "Web Subnet"
+  }
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
